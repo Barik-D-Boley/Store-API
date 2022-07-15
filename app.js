64d6e56c45080a737dd5a6b82646ef0cb3d11cd5
Barik-Boley <barik.boley@gmail.com>
2022-07-14 21:57:01 -0600

    Added link to the start log

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(errorHandler);
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`Server is listening on port ${port}`));
+        app.listen(port, console.log(`Server is listening on port ${port}\nhttp://localhost:${port}/`));
     } catch (error) { console.log(error) }
 }
 start();
